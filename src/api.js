7ccc42d16ab405b49d542a431c3179aacdc2c007
Katie Liu <kliu57@myseneca.ca>
2023-12-11 21:01:43 -0500

    added features to ui for testing more functionalities

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
- * fragments microservice (currently only running locally). We expect a user
+ * fragments microservice. We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
 export async function getUserFragments(user) {
@@ -17,11 +17,24 @@ export async function getUserFragments(user) {
     });
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
 
     const data = await res.json();
-    console.log('Got user fragments data', { data });
+    console.log('Got user fragments', { data });
+
+    // Show popup with result
+    let msg = '<title>Got user fragments!</title>';
+    msg += 'Got user fragments:<br/><br/>';
+    data.fragments.forEach(function (id) {
+      msg += `${id}<br/>`;
+    });
+
+    let newWin = window.open('', '', `width=400,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(400/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error('Unable to call GET /v1/fragments', { err });
   }
@@ -30,7 +43,7 @@ export async function getUserFragments(user) {
 /**
  * Given an authenticated user, request all fragments, expanded to include a 
  * full representation of the fragments' metadata, for this user from the
- * fragments microservice (currently only running locally). We expect a user
+ * fragments microservice. We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
 export async function getUserFragmentsExpanded(user) {
@@ -42,24 +55,41 @@ export async function getUserFragmentsExpanded(user) {
     });
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
 
     const data = await res.json();
     console.log('Got expanded user fragments data', { data });
+
+    // Show popup with result
+    let msg = '<title>Got expanded user fragments</title>';
+    msg += 'Got expanded user fragments:<br/><br/>';
+    data.fragments.forEach(function (fragment) {
+      msg += `size: ${fragment.size}<br/>`;
+      msg += `updated: ${fragment.created}<br/>`;
+      msg += `created: ${fragment.updated}<br/>`;
+      msg += `id: ${fragment.id}<br/>`;
+      msg += `ownerId: ${fragment.ownerId}<br/>`;
+      msg += `type: ${fragment.type}<br/><br/>`;
+    });
+    let newWin = window.open('', '', `width=600,height=500,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error('Unable to call GET /v1/fragments?expand=1', { err });
   }
 }
 
 /**
- * Given an authenticated user, create a text fragment for this user and 
- * save it to the fragments microservice (currently only running locally). 
+ * Given an authenticated user, create a fragment for this user and 
+ * save it to the fragments microservice. 
  * We expect a user to have an `idToken` attached, so we can send that  
  * along with the request.
  */
 export async function postFragment(user, fragmentData, fragmentDataType) {
-  console.log('Posting user fragment data...');
+  console.log(`Posting user fragment data of type ${fragmentDataType}...`);
 
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
@@ -70,59 +100,200 @@ export async function postFragment(user, fragmentData, fragmentDataType) {
     });
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
 
     const data = await res.json();
     console.log('Got posted fragment data', { data });
+
+    // Show popup with result
+    let msg = '<title>Posted user fragment!</title>';
+    msg += 'Posted user fragment:<br/><br/>';
+    msg += `id: ${data.fragment.id}<br/>`;
+    msg += `ownerId: ${data.fragment.ownerId}<br/>`;
+    msg += `created: ${data.fragment.updated}<br/>`;
+    msg += `updated: ${data.fragment.created}<br/>`;
+    msg += `type: ${data.fragment.type}<br/>`;
+    msg += `size: ${data.fragment.size}`;
+    let newWin = window.open('', '', `width=600,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error('Unable to call POST /v1/fragment', { err });
   }
 }
 
 /**
- * Given an authenticated user, get a text fragment by ID from the fragments
- * microservice (currently only running locally). We expect a user to have 
+ * Given an authenticated user, update the fragment for this user and 
+ * save it to the fragments microservice. 
+ * We expect a user to have an `idToken` attached, so we can send that  
+ * along with the request.
+ */
+export async function updateFragment(user, fragmentId, fragmentData, fragmentDataType) {
+  console.log(`Updating user fragment data of id: ${fragmentId}`);
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: "PUT",
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.postAuthorizationHeaders(fragmentDataType),
+      body: fragmentData,
+    });
+
+    if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    console.log('Updated fragment data', { data });
+
+    // Show popup with result
+    let msg = '<title>Updated user fragment!</title>';
+    msg += `Updated user fragment data of id: ${fragmentId}<br/><br/>`;
+    msg += `id: ${data.fragment.id}<br/>`;
+    msg += `ownerId: ${data.fragment.ownerId}<br/>`;
+    msg += `created: ${data.fragment.updated}<br/>`;
+    msg += `updated: ${data.fragment.created}<br/>`;
+    msg += `type: ${data.fragment.type}<br/>`;
+    msg += `size: ${data.fragment.size}`;
+    let newWin = window.open('', '', `width=600,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
+  } catch (err) {
+    console.error('Unable to call POST /v1/fragment', { err });
+  }
+}
+
+/**
+ * Given an authenticated user, get a fragment by ID from the fragments
+ * microservice. We expect a user to have 
  * an `idToken` attached, so we can send that along with the request.
  */
 export async function getFragmentById(user, fragmentId) {
   console.log(`Requesting user fragment data of id: ${fragmentId}`);
   try {
+    // Page element used for displaying response images
+    let imageDisplayElement = document.querySelector("#photo");
+    // Clear the current image displayed on page
+    imageDisplayElement.src = "";
+
     const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
-    });
+    })
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
 
-    const data = await res.json();
-    console.log(`Got user fragment data of id: ${fragmentId}`, { data });
+    let type = res.headers.get("content-type");
+    let msg = "<title>Got fragment data!</title>";
+    msg += `Got fragment data of id: ${fragmentId}<br/><br/>`;
+    msg += `Fragment type: ${type}<br/>`;
+    
+    // Check if response is an image
+    if(type.startsWith("image/")) {
+      // for images, get blob display it on page
+      let blob = await res.blob(); 
+      const blobUrl = URL.createObjectURL(blob);
+      imageDisplayElement.src = blobUrl;
+
+      msg += `Fragment data: See on page`;
+    } else {
+      let text = await res.text();
+      msg += `Fragment data: ${text}`;
+    }
+
+    console.log(`Got user fragment data of id: ${fragmentId}`);
+
+    // Show popup with result
+    let newWin = window.open('', '', `width=600,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error(`Unable to call GET /v1/fragments/${fragmentId}`, { err });
   }
 }
 
 /**
- * Given an authenticated user, get a text fragment's metadata by ID from the fragments
- * microservice (currently only running locally). We expect a user to have 
+ * Given an authenticated user, delete a fragment by ID from the fragments
+ * microservice. We expect a user to have 
+ * an `idToken` attached, so we can send that along with the request.
+ */
+export async function deleteFragment(user, fragmentId) {
+  console.log(`Deleting user fragment data of id: ${fragmentId}`);
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: "DELETE",
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+    });
+
+    if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    console.log(`Got delete result for fragment id: ${fragmentId}`, { data });
+
+    // Show popup with result
+    let msg = "<title>Deleted fragment!</title>";
+    msg += `Deleted fragment data of id: ${fragmentId}`;
+    let newWin = window.open('', '', `width=600,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
+  } catch (err) {
+    console.error(`Unable to call DELETE /v1/fragments/${fragmentId}`, { err });
+  }
+}
+
+/**
+ * Given an authenticated user, get a fragment's metadata by ID from the fragments
+ * microservice. We expect a user to have 
  * an `idToken` attached, so we can send that along with the request.
  */
 export async function getFragmentMetaById(user, fragmentId) {
   console.log(`Requesting user fragment metadata of id: ${fragmentId}`);
   try {
+    // Page element used for displaying response images
+    let imageDisplayElement = document.querySelector("#photo");
+    // Clear the current image displayed on page
+    imageDisplayElement.src = "";
+
     const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}/info`, {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
 
     const data = await res.json();
     console.log(`Got user fragment metadata of id: ${fragmentId}`, { data });
+
+    // Show popup with result
+    let msg = "<title>Got fragment metadata!</title>";
+    msg += `Got fragment metadata of id: ${fragmentId}<br/><br/>`;
+    msg += `id: ${data.fragment.id}<br/>`;
+    msg += `ownerId: ${data.fragment.ownerId}<br/>`;
+    msg += `created: ${data.fragment.updated}<br/>`;
+    msg += `updated: ${data.fragment.created}<br/>`;
+    msg += `type: ${data.fragment.type}<br/>`;
+    msg += `size: ${data.fragment.size}`;
+    let newWin = window.open('', '', `width=600,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(600/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error(`Unable to call GET /v1/fragments/${fragmentId}/info`, { err });
   }
@@ -137,11 +308,25 @@ export async function getHealthCheck() {
     const res = await fetch(`${apiUrl}/`);
 
     if (!res.ok) {
+      // Show popup with error
+      alert(`${res.statusText}`);
+
       throw new Error(`${res.status} ${res.statusText}`);
     }
     
     const data = await res.json();
     console.log('Got health check', { data });
+
+    // Show popup with result
+    let msg = "<title>Health Check successful!</title>";
+    msg += "Health Check:<br/><br/>";
+    msg += `status: ${data.status}<br/>`;
+    msg += `author: ${data.author}<br/>`;
+    msg += `githubUrl: ${data.githubUrl}<br/>`;
+    msg += `version: ${data.version}<br/>`;
+    msg += `hostname: ${data.hostname}`;
+    let newWin = window.open('', '', `width=400,height=200,top=${(screen.height/2)-(200*2)},left=${(screen.width/2)-(400/2)}`);
+    newWin.document.write(`${msg}`);
   } catch (err) {
     console.error('Unable to call GET /', { err });
   }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-import { getUserFragments, getUserFragmentsExpanded, postFragment, getFragmentById, getFragmentMetaById, getHealthCheck } from './api';
+import { getUserFragments, getUserFragmentsExpanded, postFragment, deleteFragment, updateFragment, getFragmentById, getFragmentMetaById, getHealthCheck } from './api';
 
 async function init() {
   // Get our UI elements
@@ -10,10 +10,21 @@ async function init() {
   const logoutBtn = document.querySelector('#logout');
   const getForm = document.querySelector('#get-form');
   const postForm = document.querySelector('#post-form');
-  const fragmentDataTypeDropdown = document.querySelector('#fragmentDataType')
-  const fragmentDataInputField = document.querySelector('#fragmentData')
+  const postImageForm = document.querySelector('#post-image-form');
+  const deleteForm = document.querySelector('#delete-form');
+  const updateForm = document.querySelector('#update-form');
+  const updateImageForm = document.querySelector('#update-image-form');
+  const fragmentDataTypeDropdown = document.querySelector('#fragmentDataType');
+  const fragmentDataInputField = document.querySelector('#fragmentData');
+  const fragmentImageDataInputField = document.querySelector('#fragmentImageData');
+  const updateFragmentIdInputField = document.querySelector('#updateFragmentId');
+  const updateFragmentImageIdInputField = document.querySelector('#updateFragmentImageId');
+  const updateFragmentDataTypeDropdown = document.querySelector('#updateFragmentDataType');
+  const updateFragmentDataInputField = document.querySelector('#updateFragmentData');
+  const updateFragmentImageDataInputField = document.querySelector('#updateFragmentImageData');
   const getByIdForm = document.querySelector('#get-by-id-form');
-  const fragmentIdInputField = document.querySelector('#fragmentId')
+  const fragmentIdInputField = document.querySelector('#fragmentId');
+  const deleteFragmentIdInputField = document.querySelector('#deleteFragmentId')
   const healthCheckBtn = document.querySelector('#healthCheckBtn');
 
   // Wire up event handlers to deal with login and logout.
@@ -36,14 +47,39 @@ async function init() {
     return;
   }
 
-  // Event handler to deal with "Create text fragment and store it in the fragments server" form submission
+  // Event handler to deal with "Create fragment and store it in the fragments server" form submission
   postForm.onsubmit = async (e) => {
     e.preventDefault(); // prevent the browser from automatically submitting the form
     if (user) {
       let fragmentDataType = fragmentDataTypeDropdown.value; // get the value from the dropdown field
-      let fragmentData = fragmentDataInputField.value; // get the value from the text input field
-      await postFragment(user, fragmentData, fragmentDataType);   // create the fragment and save it to db
-      fragmentDataInputField.value = '';  // clear the input field
+      let fragmentData = fragmentDataInputField.value.trim(); // get the value from the text input field
+
+      if (fragmentData.length > 0) {
+        await postFragment(user, fragmentData, fragmentDataType); // create the fragment and save it to db
+        fragmentDataInputField.value = '';  // clear the input field
+      } else {
+        alert("No data entered");
+      }
+    }
+  }
+
+  // Event handler to deal with "Create image fragment and store it in the fragments server" form submission
+  postImageForm.onsubmit = async (e) => {
+    e.preventDefault(); // prevent the browser from automatically submitting the form
+    if (user) {
+      // get the image from the file input field
+      if (document.forms['post-image-form']['fragmentImageData'].files.length > 0) {
+        let imageFile = document.forms['post-image-form']['fragmentImageData'].files[0];
+
+        imageFile.arrayBuffer().then(async buff => {
+          let imageArr = new Uint8Array(buff); // convert image to Uint8Array
+          await postFragment(user, imageArr, imageFile.type); // create the fragment and save it to db
+        });
+
+        fragmentImageDataInputField.value = '';  // clear the input field
+      } else {
+        alert("No file chosen");
+      }
     }
   }
 
@@ -63,12 +99,79 @@ async function init() {
   getByIdForm.onsubmit = async (e) => {
     e.preventDefault(); // prevent the browser from automatically submitting the form
     if (user) {
-      let fragmentId = fragmentIdInputField.value; // get the value from the text input field
+      let fragmentId = fragmentIdInputField.value.trim(); // get the value from the text input field
 
-      if (document.activeElement.value == 'Get user fragment by ID') {
-        await getFragmentById(user, fragmentId);  // get fragment from db
-      } else if (document.activeElement.value == 'Get fragment metadata by ID') {
-        await getFragmentMetaById(user, fragmentId);  // get fragment metadata from db
+      if (fragmentId.length > 0) {
+        if (document.activeElement.value == 'Get user fragment by ID') {
+          await getFragmentById(user, fragmentId);  // get fragment from db
+        } else if (document.activeElement.value == 'Get fragment metadata by ID') {
+          await getFragmentMetaById(user, fragmentId);  // get fragment metadata from db
+        }
+      } else {
+        alert("No id entered");
+      }
+    }
+  }
+
+  // Event handler to deal with "Delete an existing fragment" form submission
+  deleteForm.onsubmit = async (e) => {
+    e.preventDefault(); // prevent the browser from automatically submitting the form
+    if (user) {
+      let fragmentId = deleteFragmentIdInputField.value.trim(); // get the value from the text input field
+
+      if (fragmentId.length > 0) {
+        await deleteFragment(user, fragmentId);  // get fragment from db
+      } else  {
+        alert("No id entered");
+      }
+    }
+  }
+
+  // Event handler to deal with "Update an existing fragment" form submission
+  updateForm.onsubmit = async (e) => {
+    e.preventDefault(); // prevent the browser from automatically submitting the form
+    if (user) {
+      let fragmentId = updateFragmentIdInputField.value.trim(); // get the value from the text input field
+
+      if (fragmentId.length > 0) {
+        let fragmentDataType = updateFragmentDataTypeDropdown.value; // get the value from the dropdown field
+        let fragmentData = updateFragmentDataInputField.value.trim(); // get the value from the text input field
+
+        if (fragmentData.length > 0) {
+          await updateFragment(user, fragmentId, fragmentData, fragmentDataType); // update the fragment
+          updateFragmentIdInputField.value = ''; // clear the id field
+          updateFragmentDataInputField.value = '';  // clear the input field
+        } else {
+          alert("No data entered");
+        }
+      } else {
+        alert("No id entered");
+      }
+    }
+  }
+
+  // Event handler to deal with "Update an existing image fragment" form submission
+  updateImageForm.onsubmit = async (e) => {
+    e.preventDefault(); // prevent the browser from automatically submitting the form
+    if (user) {
+      let fragmentId = updateFragmentImageIdInputField.value.trim(); // get the value from the text input field
+
+      if (fragmentId.length > 0) {
+        // get the image from the file input field
+        if (document.forms['update-image-form']['updateFragmentImageData'].files.length > 0) {
+          let imageFile = document.forms['update-image-form']['updateFragmentImageData'].files[0];
+
+          imageFile.arrayBuffer().then(async buff => {
+            let imageArr = new Uint8Array(buff); // convert image to Uint8Array
+            await updateFragment(user, fragmentId, imageArr, imageFile.type); // update the fragment
+            updateFragmentImageIdInputField.value = ''; // clear the id field
+            updateFragmentImageDataInputField.value = '';  // clear the input field
+          });
+        } else {
+          alert("No file chosen");
+        }
+      } else {
+        alert("No id entered");
       }
     }
   }
@@ -92,7 +195,7 @@ async function init() {
   loginBtn.disabled = true;
 
   // Do an authenticated request to the fragments API server and log the result
-  await getUserFragments(user);
+  await getUserFragmentsExpanded(user);
 }
 
 // Wait for the DOM to be ready, then start the app
